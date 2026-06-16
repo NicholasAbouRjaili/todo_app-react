327cdd575416eca1f22386f8ada1c4d83007af4d
NicholasAbouRjaili <narjaili@aldogroup.com>
2026-06-16 09:27:08 -0400

    Add MyButton component and MyApp function

--- script.jsx
+++ script.jsx
@@ -1,1 +1,13 @@
-
+function MyButton ({ title }: { title : string}) {
+        return (
+            <button>title</button>
+        ):
+    }
+    
+    export default function MyApp(){
+        return (
+            <div>
+            <MyButton title ="Add task" />
+            </div>
+        );
+    }
