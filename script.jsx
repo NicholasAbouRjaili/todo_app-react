4f330ce2876936aeda48eeddccf698f1ac9d5ae6
NicholasAbouRjaili <narjaili@aldogroup.com>
2026-06-16 09:28:53 -0400

    Update script.jsx

--- script.jsx
+++ script.jsx
@@ -1,13 +1,13 @@
 function MyButton ({ title }: { title : string}) {
-        return (
-            <button>title</button>
-        ):
-    }
-    
-    export default function MyApp(){
-        return (
-            <div>
-            <MyButton title ="Add task" />
-            </div>
-        );
-    }
+    return (
+        <button>{title}</button>
+    );
+}
+
+export default function MyApp(){
+    return (
+        <div>
+            <MyButton title="Add task" />
+        </div>
+    );
+}
